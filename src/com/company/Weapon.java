7b0f4952de37e1e1e933fc941e8735751058e69b
MATTIA.CONCI <MATTIA.CONCI@buonarroti.tn.it>
2022-11-16 15:59:36 +0100

    aggiunti metodi getter e setter della classe Weapon

--- src/com/company/Weapon.java
+++ src/com/company/Weapon.java
@@ -3,4 +3,20 @@ package com.company;
 public class Weapon {
     private int dannoArma;
     private String nomeArma;
+
+    public int getDannoArma() {
+        return dannoArma;
+    }
+
+    public String getNomeArma() {
+        return nomeArma;
+    }
+
+    public void setDannoArma(int dannoArma) {
+        this.dannoArma = dannoArma;
+    }
+
+    public void setNomeArma(String nomeArma) {
+        this.nomeArma = nomeArma;
+    }
 }
