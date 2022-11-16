624e8755cc4c76ad154c78a1b7765c88e5b29978
MATTIA.CONCI <MATTIA.CONCI@buonarroti.tn.it>
2022-11-16 15:20:55 +0100

    fix: migliorata interfaccia utente

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,21 +7,25 @@ public class Main {
         System.out.println("Se ni’ mondo esistesse un po’ di bene e ognun si honsiderasse suo fratello ci sarebbe meno pensieri e meno pene e il mondo ne sarebbe assai più bello");
         boolean turno=false;
         Random rand=new Random();
-        Monster mostro1= new Monster("Pacciani",100,5);
-        Monster mostro2= new Monster("Fantasma",50,10);
+        Monster mostro1= new Monster("Yeti",1000,50);
+        Monster mostro2= new Monster("bigfoot",500,100);
         while(mostro1.getPuntiFerita()>0||mostro2.getPuntiFerita()>0){
             turno= rand.nextBoolean();
             if(turno){
                 mostro1.subisci(mostro2.attacca());
+                System.out.println(" "+mostro2.getNome()+"usa fuocobomba su"+mostro1.getNome());
             }
             else{
                 mostro2.subisci(mostro1.attacca());
+                System.out.println(" "+mostro2.getNome()+"utilizza abisso su"+mostro1.getNome());
             }
         }
         if(mostro1.getPuntiFerita()==0){
+            System.out.println(mostro1.getNome()+" è esausto...");
             System.out.println("il mostro vincitore è "+mostro2.getNome());
         }
         else{
+            System.out.println(mostro2.getNome()+" è esausto...");
             System.out.println("il mostro vincitore è "+mostro1.getNome());
         }
         
